20432a79bff49e6d374b2424f2ca4c6a5d27e31f
Mustafa Battal <battalmustafa88@gmail.com>
2022-05-31 16:44:57 +0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,47 @@
 import { useEffect, useState } from "react";
 import OfferList from "./OfferList";
+import NumOffers from "./NumOffers";
 const Home = () => {
-  const [offers, setOffers] = useState(null)
-
+  const [numOffers, setNumOffers] = useState(null)
+  const [offers, setOffers] = useState(null);
+  const [isPending, setIsPending] = useState(true);
   useEffect(() => {
-    fetch('https://snetmyapp.herokuapp.com/case1')
+    fetch('https://snetmyapp.herokuapp.com/get_offer_count')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => { 
+        setNumOffers(data);
+        //console.log(data)
+      })
+      fetch ('https://snetmyapp.herokuapp.com/case3')
       .then(res => {
         return res.json();
       })
       .then(data => {
         setOffers(data);
-        //console.log(data)
+        setIsPending(false);
+       
+        console.log(data)
       })
-  }, [])
-
+  
+    }, [])
+  
+  
+  
+ 
   return (
     <div className="home">
-      {offers && <OfferList offers={offers.offerList} />}
-    
+       { isPending && <div class="d-flex align-items-center">
+  <strong>Teklifler Hazırlanıyor...</strong>
+  <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+</div> }
+{ numOffers && <div class="d-flex align-items-center">
+  <strong>{numOffers.num_offers}adet Teklif Hazırlanıyor...</strong>
+  
+</div> }  
+    {offers && <OfferList offers={offers.offerList} />}
+
     
     </div>
   );
